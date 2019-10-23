66c54eb6b255b00c817a56e8c1ea2101c4e812ee
mmelligy <mahmoud-elmelligy@elarabygroup.com git config --global user.email mahmoud-elmelligy@elarabygroup.com>
2019-10-23 19:14:39 +0200

    Data persistent using user Defaults methods

--- ToDoey/TodoListViewController.swift
+++ ToDoey/TodoListViewController.swift
@@ -12,9 +12,14 @@ class TodoListViewController : UITableViewController{
     
     var itemArray = ["Find Mike","Buy Eggod","Destory Demogorgon"]
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodolistArray") as? [String]{
+            itemArray = items
+        }
         
     }
     
@@ -56,6 +61,8 @@ class TodoListViewController : UITableViewController{
             //what will happen once the user clicks the add item button on our alert
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodolistArray")
             
             self.tableView.reloadData()
         }
